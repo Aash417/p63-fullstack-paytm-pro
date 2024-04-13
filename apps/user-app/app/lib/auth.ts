a36c3506f02024382ccd9adccdf98cb57a81c5c4
aashish_k <aashishkathat00@gmail.com>
2024-04-14 00:32:38 +0530

    fixed user auth issue

--- apps/user-app/app/lib/auth.ts
+++ apps/user-app/app/lib/auth.ts
@@ -10,9 +10,11 @@ export const authOptions = {
 				phone: { label: 'Phone number', type: 'text', placeholder: '1231231231' },
 				password: { label: 'Password', type: 'password' },
 			},
+
 			// TODO: User credentials type from next-aut
 			async authorize(credentials: any) {
 				// Do zod validation, OTP validation here
+				console.log(credentials);
 				const hashedPassword = await bcrypt.hash(credentials.password, 10);
 				const existingUser = await db.user.findFirst({
 					where: {
