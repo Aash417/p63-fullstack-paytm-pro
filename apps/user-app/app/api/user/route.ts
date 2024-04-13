676797140f808d1caac7713b8c1ef007a05f9f81
aashish_k <aashishkathat00@gmail.com>
2024-04-13 16:10:57 +0530

    added a route for testing db conn

---
+++ apps/user-app/app/api/user/route.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from '@repo/db/client';
+import { NextResponse } from 'next/server';
+const client = new PrismaClient();
+
+export const GET = async () => {
+	await client.user.create({
+		data: {
+			email: 'one',
+			name: 'one',
+		},
+	});
+
+	return NextResponse.json({
+		msg: 'Created entry in db.',
+	});
+};
