df03f4a3ac1727f9519db1bcca28563a23275a4a
aashish_k <aashishkathat00@gmail.com>
2024-04-14 20:20:09 +0530

    added auth at root

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,11 +1,3 @@
-import { Button } from '@repo/ui/button';
-import styles from './page.module.css';
-
-
 export default function Page(): JSX.Element {
-	return (
-		<Button appName='web' className={`${styles.button} text-5xl`}>
-			Click me!
-		</Button>
-	);
+	return <div className=''>user app</div>;
 }
